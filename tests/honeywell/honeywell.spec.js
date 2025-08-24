f962471f601e5ab5d1e99d4863c58b1798994fda
Caleb Steinwandt <jsteinca@jci.com>
2025-08-24 06:49:53 -0600

    standardize honeywell with other tests

--- tests/honeywell/honeywell.spec.js
+++ tests/honeywell/honeywell.spec.js
@@ -1,4 +1,14 @@
 import {test, expect} from '@playwright/test';
+import { devices } from './honeywellDevices';
+
+const {wll, whl, wol, leak} = devices
+const {fill, drain} = devices;
+const {faceDamper, bypassDamper} = devices;
+const {primary, secondary} = devices;
+const {sump, bleed, conductivity} = devices;
+const {rh1, rh2, maTemp, saTemp} = devices;
+const {vfd, vfdEnable, vfdFault, vfdHOA, airflow} = devices;
+const {sf1, sf2,sf3, sf4, sf5, sf6} = devices;
 
 const controllerIp = "192.168.200.34";
 const cpnx = '/ord/station:%7Cslot:/Drivers/OnboardIONetwork/CPNX/points/AHU%7Cview:webEditors:MultiSheet'
@@ -34,38 +44,38 @@ test.afterAll(async () => {
   
 test.describe('low voltage', () => {
 	test('spot leak', async  () => {
-		await testBinaryDevice("WS01", /^Normal/, /^Alarm/)
+		await testBinaryDevice(leak, /^Normal/, /^Alarm/)
 	})
 	test('fill', async()=>{
-		await testBinaryIO('LCV01', /^Closed/, /^Open/)
+		await testBinaryIO(fill, /^Closed/, /^Open/)
 	})
 	test('drain', async()=>{
-		await testBinaryIO('LCV02', /^Closed/, /^Open/)
+		await testBinaryIO(drain, /^Closed/, /^Open/)
 	})
 	test('whl', async  () => {
 		await navigateToPage(cpv_1)
-		await testBinaryDevice("LH01_HL", /^Normal/, /^Warning/)
+		await testBinaryDevice(whl, /^Normal/, /^Warning/)
 	})
 	test('wol', async  () => {
-		await testBinaryDevice("LN01_NL", /^Off/, /^On/)
+		await testBinaryDevice(wol, /^Off/, /^On/)
 	})
 	test('wll', async  () => {
-		await testBinaryDevice("LL01_LL", /^Warning/, /^Normal/)
+		await testBinaryDevice(wll, /^Warning/, /^Normal/)
 	})
 	test('face damper', async () => {
-		await commandDamper('ND01', "0")
-		await commandDamper('ND01', "50")
-		await commandDamper('ND01', "100")
+		await commandDamper(faceDamper, "0")
+		await commandDamper(faceDamper, "50")
+		await commandDamper(faceDamper, "100")
 	})
 	test('bypass damper', async () => {
-		await commandDamper('ND02', "0")
-		await pollAnalog("ND02", 95, 100)
+		await commandDamper(bypassDamper, "0")
+		await pollAnalog(bypassDamper, 95, 100)
 		
-		await commandDamper('ND02', "50")
-		await pollAnalog("ND02", 45, 55)
+		await commandDamper(bypassDamper, "50")
+		await pollAnalog(bypassDamper, 45, 55)
 		
-		await commandDamper('ND02', "100")
-		await pollAnalog("ND02", 0, 5)
+		await commandDamper(bypassDamper, "100")
+		await pollAnalog(bypassDamper, 0, 5)
 
 	})
 })
@@ -75,67 +85,78 @@ async function navigateToPage(segment){
 	await expect(page.locator('.ux-table')).toBeVisible({timeout: 0})
 
 }
-async function pollAnalog(device , lowRange, highRange){
-	let duration = 2 *60000
-	await expect.poll(async () => {
-		let fbk = parseFloat(await page.locator('.ux-table').locator('tr', {hasText: device}).locator('td').nth(1).textContent())
-		return fbk >= lowRange && fbk <= highRange
-		}, {
-		timeout: duration
-	}).toBe(true);
-	await page.waitForTimeout(10000);
-	console.log(parseFloat(device, await page.locator('.ux-table').locator('tr', {hasText: device}).locator('td').nth(1).textContent()))
-}
 async function commandDamper(device, command){
 	await navigateToPage(cpv_1)
 	await commandAnalogDevice(device, command)
 	await navigateToPage(cpnx)
 }
 async function getBinaryInput (device, state){
-	await expect(page.locator('.ux-table').locator('tr', {hasText: device}).locator('td').nth(1)).toHaveText(state)
+	const {feedbackValue, commandValue, name} = device;
+	await expect(page.locator('.ux-table').locator('tr', {hasText: feedbackValue}).locator('td').nth(1)).toHaveText(state)
 	console.log(device, state)
 }
+
+async function getAnalogInput(device){
+	const {name, commandValue, feedbackValue} = device;
+	let value = parseFloat(await page.locator('.ux-table').locator('tr', {hasText: feedbackValue}).locator('td').nth(1).textContent());
+	console.log(`${name} reading: ${value}`)
+	return value;
+}
+
 async function testBinaryDevice(device, state1, state2){
 	await getBinaryInput(device, state1);
 	await getBinaryInput(device, state2)
 }
 async function commandBinaryDevice (device, value){
-	await page.locator('.ux-table').locator('tr', {hasText: device + '_OC'}).locator('td').nth(2).getByRole("button").click()
+	const {feedbackValue, commandValue, name} = device;
+	await page.locator('.ux-table').locator('tr', {hasText: commandValue}).locator('td').nth(2).getByRole("button").click()
 	await page.getByRole('button', { name: value }).click()
 	await page.getByRole('button', { name: 'Ok' }).click()
-	await expect(page.locator('.ux-table').locator('tr', {hasText: device + '_STS'}).locator('td').nth(1)).toHaveText(value)
+	await expect(page.locator('.ux-table').locator('tr', {hasText: feedbackValue}).locator('td').nth(1)).toHaveText(value)
 	console.log(device, value)
 }
 
-async function testBinaryIO(device, value1, value2){
-	await commandBinaryDevice(device, value1)
-	await commandBinaryDevice(device, value2)
-}
-
 async function commandAnalogDevice(device, value){
+	const {feedbackValue, commandValue, name} = device;
 	await page.waitForTimeout(2000)
-	await page.locator('.ux-table').locator('tr', {hasText: device + '_CMD'}).locator('td').nth(2).getByRole("button").click()
+	await page.locator('.ux-table').locator('tr', {hasText: commandValue}).locator('td').nth(2).getByRole("button").click()
 	await page.getByRole('button', { name: "Override" }).click()
 	await page.getByRole('textbox').fill(value)
 	await page.getByRole('button', { name: 'Ok' }).click()
 	console.log(device, value)
 }
-async function getAnalogFeedback(device, command, range){
+async function testBinaryIO(device, value1, value2){
+	await commandBinaryDevice(device, value1)
+	await commandBinaryDevice(device, value2)
+}
+
+async function testAnalogInput(device, command){
+	const {feedbackValue, commandValue, name} = device;
 	let result;
-	let value = parseFloat(await page.locator('.ux-table').locator('tr', {hasText: device + '_FB'}).locator('td').nth(1).textContent());
-	expect(value).toBeGreaterThanOrEqual(0);
-	console.log(`${device} initial reading: ${value}`)
+	getAnalogInput(device)
 	for (let i = 0; i < 400; i++) {
-		let feedback = await page.locator('.ux-table').locator('tr', {hasText: device + '_FB'}).locator('td').nth(1).textContent();
+		let feedback = getAnalogInput(device)
 		console.log(feedback)
 		result = parseFloat(feedback);
 		console.log(result)
-		if (Math.abs(command - result) < range ) {
+		if (Math.abs(command - result) < 0.1 ) {
 			await page.waitForTimeout(500);
 			console.log(`${device} feedback: ${feedback}`);
 			break;
 		}
-		feedback = await page.locator('.ux-table').locator('tr', {hasText: device + '_FB'}).locator('td').nth(1).textContent();
+		feedback = getAnalogInput(device);
 		await new Promise(r => setTimeout(r, 7000));
 	}
 }
+
+// async function pollAnalog(device , lowRange, highRange){
+// 	let duration = 2 *60000
+// 	await expect.poll(async () => {
+// 		let fbk = parseFloat(await page.locator('.ux-table').locator('tr', {hasText: device}).locator('td').nth(1).textContent())
+// 		return fbk >= lowRange && fbk <= highRange
+// 		}, {
+// 		timeout: duration
+// 	}).toBe(true);
+// 	await page.waitForTimeout(10000);
+// 	console.log(parseFloat(device, await page.locator('.ux-table').locator('tr', {hasText: device}).locator('td').nth(1).textContent()))
+// }
---
+++ tests/honeywell/honeywellDevices.js
@@ -0,0 +1,47 @@
+class Device{
+	constructor(name, feedbackValue = "FB", commandValue ="OC"){
+		this.name = name;
+		this.feedbackValue = name + `_${feedbackValue}`;
+		this.commandValue = name + `_${commandValue}`;
+	}
+}
+
+export const devices = {
+	fill: new Device("LCV01"),
+	drain: new Device("LCV02"),
+
+	rh1: new Device("NT01", "SAH"),
+	rh2: new Device("NT02", "SAH"),
+	saTemp: new Device("TT01", "SAT"),
+	maTemp: new Device("TT02", "MAT"),
+	
+	faceDamper: new Device("ND01", "FB", "CMD"),
+	bypassDamper: new Device("ND02", "FB", "CMD"),
+	
+	whl: new Device("LH01", "HL"),
+	wol: new Device("LN01", "NL"),
+	wll: new Device("LL01", "LL"),
+	
+	leak: new Device("WS01", "WD"),
+	
+	sump: new Device("EP01", "STS", "SS"),
+	conductivity: new Device("CT01", "CON"),
+	bleed: new Device("CCV01", undefined, "OC"),
+	
+	sf1: new Device("SF01", "STS"),
+	sf2: new Device("SF02", "STS"),
+	sf3: new Device("SF03", "STS"),
+	sf4: new Device("SF04", "STS"),
+	sf5: new Device("SF05", "STS"),
+	sf6: new Device("SF06", "STS"),
+	
+	vfd: new Device ("SC01", "FB", "SPD"),
+	vfdEnable: new Device("SC01", undefined, "SS"),
+	vfdFault: new Device("SC01", "FLT"),
+	vfdHOA: new Device("SC01", "STS"),
+	
+	airflow: new Device("FT01", "SAF"),
+	
+	primary: new Device("QS01", "PA"),
+	secondary: new Device("QS02", "PA"),
+}
