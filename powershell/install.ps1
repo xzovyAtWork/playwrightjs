231333ebe8140cfab06471d8bd46582ba7d08a0e
Caleb Steinwandt <jsteinca@jci.com>
2025-09-15 09:39:26 -0600

    update install script

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -1,14 +1,13 @@
-# $location = "C:\Users\$env:USERNAME\Documents"
-# New-Item $location
-# Set-Location $location
-# Invoke-WebRequest 'https://github.com/xzovyAtWork/playwrightjs/archive/refs/heads/master.zip' -OutFile .\playwright.zip
-# Write-Host "repository fetched"
-# Expand-Archive .\playwright.zip .\
-# Rename-Item .\playwrightjs-master .\playwrightjs
-# Remove-Item .\playwright.zip
-# Set-Location .\playwrightjs
-# Write-Host "Initializing playwright..."
-# npm init playwright@latest
+$location = "C:\Users\$env:USERNAME\Documents"
+Set-Location $location
+Invoke-WebRequest 'https://github.com/xzovyAtWork/playwrightjs/archive/refs/heads/master.zip' -OutFile .\playwright.zip
+Write-Host "repository fetched"
+Expand-Archive .\playwright.zip .\
+Rename-Item .\playwrightjs-master .\playwright
+Remove-Item .\playwright.zip
+Set-Location .\playwright
+Write-Host "Initializing project..."
+npm init -y
 Write-Host "Installing @playwright/test..."
 npm install -D @playwright/test@latest
 Write-Host "Installing chromium..."
